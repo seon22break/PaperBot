495f1d0ed21ed312c178585e33a3a7a4db80e45f
Jhonatan Matias Martin <jhonatanmatiasmartinmartin@outlook.es>
2022-01-20 13:08:58 +0100

    Text in config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ URL_ERROR = "La url no está permitida"
 
 CONTENT_ERROR = "No se ha podido procesar el contenido."
 
-HELP = '''Este bot ha sido creado para poder leer algunos articulos que se 
-encuentran en los principales periodicos españoles y que contienen algún tipo de prohibición 
-(*suscripción* o *bloqueadores de anuncios*) 
-y nos devuelve el texto de la noticia, si esta se encuentra renderizada'''
+HELP = '''Ey!! Que pasa!!! Estas en mi bot y lo que quieres es disfrutar de la prensa
+escrita sin restricciones (aunque deberías abonar la suscripción si quieres que siga existiendo)
+aún así estas en el sitio correcto. Manda un mensaje con la *dirección web* de la noticia
+y te llegará el contenido de la misma. Si quieres mas info visita el repositorio : https://github.com/seon22break/PaperBot '''
